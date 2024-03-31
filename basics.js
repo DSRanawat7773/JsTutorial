81afdf1ed86ef9ebe3c8ca612400e1e0c88a55d1
DSRanawat7773 <dalpatsaranawat@gmail.com>
2024-03-31 17:47:32 +0530

    Datatype

--- basics.js
+++ basics.js
@@ -6,5 +6,10 @@ accountCity = "Jaipur"
 
 console.log(accountId);
 
-accountId = 789456;
-console.log(accountId);
+// accountId = 789456;  NOT ALLOWED FOR CONST VARIABLE
+accountEmail = "Dalpat@google.com"
+accountPass = "546888"
+accountCity = "Bhilwara"
+console.table([accountId, accountEmail, accountPass, accountCity])
+
+/* prefer not to use var due to the block scope and functional scope */
