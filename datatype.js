a22e13373c3158318b0d75c30f270f502e6aa1cf
DSRanawat7773 <dalpatsaranawat@gmail.com>
2024-03-31 18:15:48 +0530

    Datatype2

---
+++ datatype.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all js JS code as newer version
+
+// alert(3+3) we are using nodejs not browser
+
+let name = "Dalpat"
+let num = 1234567899
+let bool = false
+
+console.log(typeof num)
