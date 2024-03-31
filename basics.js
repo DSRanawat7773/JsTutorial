8fb9c6dab7a9e3e979c88f61fe482c4efa97a490
DSRanawat7773 <dalpatsaranawat@gmail.com>
2024-03-31 17:07:10 +0530

    ii

--- basics.js
+++ basics.js
@@ -1,1 +1,10 @@
 const accountId = 123456;
+let accountEmail = "dsrana@gmail.com"
+var accountPass = "124536"
+accountCity = "Jaipur"
+
+
+console.log(accountId);
+
+accountId = 789456;
+console.log(accountId);
