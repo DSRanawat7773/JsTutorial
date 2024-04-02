d74c254911b328480b348143422577867110f11f
DSRanawat7773 <dalpatsaranawat@gmail.com>
2024-04-02 18:06:00 +0530

    Date And Time

---
+++ date_time.js
@@ -0,0 +1,14 @@
+//DATES
+
+let mydate = new Date();
+/* console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.getTime());
+console.log(mydate.getTime());
+console.log(mydate.toLocaleTimeString());
+console.log(mydate.getTimezoneOffset());
+*/
+
+let myCreateddate = new Date("2023, 08, 11")
+console.log(myCreateddate.toLocaleString());
+console.log(myCreateddate.toUTCString());
