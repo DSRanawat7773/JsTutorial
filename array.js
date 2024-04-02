f38a841ad0ab8e3c869073ae1305b56819003ace
DSRanawat7773 <dalpatsaranawat@gmail.com>
2024-04-02 22:06:43 +0530

    Array

---
+++ array.js
@@ -0,0 +1,16 @@
+//Array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+const myHeroes = ["Shaktiman", "Spiderman"]
+
+const myArr2 = new Array(1,2,3,4);
+
+myArr2.push(88);
+myArr2.pop();
+myArr2.unshift(2);
+console.log(myArr2);
+
+
+
+
