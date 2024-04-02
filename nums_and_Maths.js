35747146ab4a4b8a3e0140b8a271a58a2aa99b45
DSRanawat7773 <dalpatsaranawat@gmail.com>
2024-04-02 17:40:13 +0530

    Nums and Maths

---
+++ nums_and_Maths.js
@@ -0,0 +1,22 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance)
+
+// console.log(balance.toString.length)
+// console.log(balance.toFixed(2))
+
+/* ------MATHS----- */
+console.log(Math)
+console.log(Math.round(4.5))
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 5;
+const max = 10;
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
+
---
+++ string.js
@@ -0,0 +1,6 @@
+let name = "Dalpat"
+let age  = 21
+
+// console.log(name+age+ " value")
+
+console.log(`My name is ${name} and age is ${age}`)
