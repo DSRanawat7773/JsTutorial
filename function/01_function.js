6b2543ea52a8f37477571e1bbaf9b798b78e1d8d
DSRanawat7773 <dalpatsaranawat@gmail.com>
2024-04-04 01:33:48 +0530

    functions

---
+++ function/01_function.js
@@ -0,0 +1,18 @@
+
+function sayMyName(){
+    console.log("D");
+    console.log("A");
+    console.log("L");
+    console.log("P");
+    console.log("A");
+    console.log("T");
+}
+
+// sayMyName();
+
+function addNumbers(number1, number2){
+    return number1 + number2;
+}
+
+const result = addNumbers(5, 9);
+console.log(`Result : ${result}`);
