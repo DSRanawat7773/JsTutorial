8fc07b0491a61e1853ad6fb573228c88df4371ad
DSRanawat7773 <dalpatsaranawat@gmail.com>
2024-04-04 17:03:13 +0530

    if-else

---
+++ controlFlow/one.js
@@ -0,0 +1,27 @@
+
+//IF
+
+// const temperature = 40;
+
+// if(temperature === 40){
+//     console.log("Executed")
+// }
+// else{
+//     console.log("Not executed")
+// }
+
+const score = 200;
+
+// if(score>100){
+//     let power = "Fly";
+//     console.log(`User's power : ${power}`);
+// }
+
+// console.log(power)   /*GIVES ERROR DUE TO  THE SCOPE*/   
+
+const userLoggedIn = true;
+const debitCard = false;
+
+if(userLoggedIn && debitCard && 2==2){
+    console.log("Allow to buy course");
+}
